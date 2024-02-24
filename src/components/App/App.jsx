1688643985e21dff09b9e409c0707337eb9a1c77
Liubov2909 <ostapcuk2909@gmail.com>
2024-02-24 17:20:18 +0200

    dz 3 react parttask

---
+++ src/components/App/App.jsx
@@ -0,0 +1,46 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactList from "../ContactList/ContactList";
+import css from "./App.module.css";
+
+function App() {
+  const initialConttacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
+
+  const [contacts, setContacts] = useState(initialConttacts);
+  const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [
+      ...prevContacts,
+      { ...newContact, id: nanoid() },
+    ]);
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    );
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div className={css.box}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <SearchBox changeInput={setFilter} value={filter} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    </div>
+  );
+}
+
+export default App;
